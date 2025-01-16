7a9e54912e34194c9567e2e07c19c7ac989d8981
Maxim <maxkforworks@gmail.com>
2025-01-16 12:25:25 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import './App.css';
+import Video from './Components/Video';
+
+function App() {
+  return (
+    <div className="App">
+      <Video />
+    </div>
+  );
+}
+
+export default App;
